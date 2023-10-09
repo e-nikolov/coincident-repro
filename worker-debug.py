86852f55a0f6f4b56f1f03275e58512f7f1f8128
e-nikolov <emil.e.nikolov@gmail.com>
2023-10-09 13:32:14 +0200

    Updates

--- worker-debug.py
+++ worker-debug.py
@@ -1,21 +1,16 @@
-print("main.py")
+print("worker-debug.py")
 
 # pyright: reportMissingImports=false
 from polyscript import xworker
 
 
 def py_func():
-    print("py_func")
-
     return True
 
 
 xworker.sync.py_func = py_func
 
 xworker.sync.js_on_worker_ready()
-
-xworker.sync.js_func()
-
 
 for i in range(100000):
     xworker.sync.js_func()
